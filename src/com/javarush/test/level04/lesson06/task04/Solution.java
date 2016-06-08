ab4cf0269f89892e8c7227bb96fa11e9f51bb28c
motoyarko <motoyarko@gmail.com>
2016-06-08 18:40:57 +0300

    level 04 lesson 04

--- src/com/javarush/test/level04/lesson06/task04/Solution.java
+++ src/com/javarush/test/level04/lesson06/task04/Solution.java
@@ -10,7 +10,18 @@ public class Solution
 {
     public static void main(String[] args) throws Exception
     {
-        //напишите тут ваш код
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String name1 = bufferedReader.readLine();
+        String name2 = bufferedReader.readLine();
+
+        if (name1.equals(name2)){
+            System.out.println("Имена идентичны");
+        }
+        else {
+            if (name1.length() == name2.length()){
+                System.out.println("Длины имен равны");
+            }
+        }
 
     }
 }
