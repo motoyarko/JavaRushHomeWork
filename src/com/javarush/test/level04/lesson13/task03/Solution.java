65cbf35bbe3cf9d5e176ed544c724633c655377d
motoyarko <motoyarko@gmail.com>
2016-06-09 18:46:48 +0300

    level 04 lesson 13

---
+++ src/com/javarush/test/level04/lesson13/task03/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level04.lesson13.task03;
+
+import java.io.*;
+
+/* Рисуем треугольник
+Используя цикл for вывести на экран прямоугольный треугольник из восьмёрок со сторонами 10 и 10.
+Пример:
+8
+88
+888
+...
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        for (int i = 0; i < 11; i++){
+            for (int k = 0; k < i; k++){
+                System.out.print(8);
+            }
+            System.out.println();
+        }
+
+    }
+}
