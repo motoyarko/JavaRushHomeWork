72a901507ae1d9378e3a63c778e4efded68224f2
motoyarko <motoyarko@gmail.com>
2016-06-10 12:30:41 +0300

    Level 05 Lesson 07 task 5

--- src/com/javarush/test/level05/lesson07/task05/Rectangle.java
+++ src/com/javarush/test/level05/lesson07/task05/Rectangle.java
@@ -11,6 +11,33 @@ package com.javarush.test.level05.lesson07.task05;
 
 public class Rectangle
 {
-    //напишите тут ваш код
+    int left, top, width, height;
+
+
+    public void initialize(int left, int top, int width, int height){
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+    }
+    public void initialize(int left, int top){
+        this.left = left;
+        this.top = top;
+        this.width = 0;
+        this.height = 0;
+
+    }
+    public void initialize(int left, int top, int width){
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = width;
+    }
+    public void initialize(Rectangle rectangle){
+        this.left = rectangle.left;
+        this.top = rectangle.top;
+        this.width = rectangle.width;
+        this.height = rectangle.height;
+    }
 
 }
