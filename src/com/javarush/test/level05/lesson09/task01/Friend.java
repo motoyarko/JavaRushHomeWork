fee052d7dfb6811b66897135cda9f8e17bbda070
motoyarko <motoyarko@gmail.com>
2016-06-13 20:00:39 +0300

    Level 05 Lesson 07 task 5

---
+++ src/com/javarush/test/level05/lesson09/task01/Friend.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level05.lesson09.task01;
+
+/* Создать класс Friend
+Создать класс Friend (друг) с тремя конструкторами:
+- Имя
+- Имя, возраст
+- Имя, возраст, пол
+*/
+
+public class Friend
+{
+    //напишите тут ваш код
+
+}
