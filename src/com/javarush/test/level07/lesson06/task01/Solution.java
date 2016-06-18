526737b1fc0e15eb8d9200d14a38f0d6449c59c2
motoyarko <motoyarko@gmail.com>
2016-06-19 00:08:41 +0300

    level07.lesson06

---
+++ src/com/javarush/test/level07/lesson06/task01/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.test.level07.lesson06.task01;
+
+import java.util.ArrayList;
+
+/* 5 различных строчек в списке
+1. Создай список строк.
+2. Добавь в него 5 различных строчек.
+3. Выведи его размер на экран.
+4. Используя цикл выведи его содержимое на экран, каждое значение с новой строки.
+*/
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("test1");
+        list.add("test2");
+        list.add("test3");
+        list.add("test4");
+        list.add("test5");
+
+        System.out.println(list.size());
+
+        for (String i : list)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
