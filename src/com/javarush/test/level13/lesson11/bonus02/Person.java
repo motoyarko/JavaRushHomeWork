33a120872863359cdfa90614f1566f7f5de62195
Yaroslav <motoyarko@gmail.com>
2016-07-06 17:58:20 +0300

    Level13.lesson11

--- src/com/javarush/test/level13/lesson11/bonus02/Person.java
+++ src/com/javarush/test/level13/lesson11/bonus02/Person.java
@@ -1,6 +1,6 @@
 package com.javarush.test.level13.lesson11.bonus02;
 
-public class Person
+public class Person implements RepkaItem
 {
     private String name;
     private String namePadezh;
@@ -9,5 +9,20 @@ public class Person
     {
         this.name = name;
         this.namePadezh = namePadezh;
+    }
+
+    @Override
+    public String getNamePadezh()
+    {
+        char[] s = this.namePadezh.toCharArray();
+        s[s.length-1] = 'у';
+        String.valueOf(s);
+
+        return String.valueOf(s);
+    }
+
+    public void pull(Person person)
+    {
+        System.out.println(this.name + " за " + person.getNamePadezh());
     }
 }
