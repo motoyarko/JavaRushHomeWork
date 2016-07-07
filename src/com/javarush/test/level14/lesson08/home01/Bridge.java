7a450e612ec58d38393758367a58f91abcb98da4
motoyarko <motoyarko@gmail.com>
2016-07-07 22:40:39 +0300

    level14.lesson08

---
+++ src/com/javarush/test/level14/lesson08/home01/Bridge.java
@@ -0,0 +1,9 @@
+package com.javarush.test.level14.lesson08.home01;
+
+/**
+ * Created by motoy on 7/7/2016.
+ */
+public interface Bridge
+{
+    int getCarsCount();
+}
---
+++ src/com/javarush/test/level14/lesson08/home01/WaterBridge.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level14.lesson08.home01;
+
+
+
+/**
+ * Created by motoy on 7/7/2016.
+ */
+public class WaterBridge implements Bridge
+{
+    @Override
+    public int getCarsCount()
+    {
+        return 8;
+    }
+}
