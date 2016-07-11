42d1f1331307ed24fb9c049ecc4126fbd0ddbc3a
motoyarko <motoyarko@gmail.com>
2016-07-11 21:03:27 +0300

    level14.lesson08

--- src/com/javarush/test/level14/lesson08/bonus02/Solution.java
+++ src/com/javarush/test/level14/lesson08/bonus02/Solution.java
@@ -6,9 +6,17 @@ package com.javarush.test.level14.lesson08.bonus02;
 Вывести в консоль наибольший общий делитель.
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution
 {
     public static void main(String[] args) throws Exception
     {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bufferedReader.readLine());
+        int b = Integer.parseInt(bufferedReader.readLine());
+
+        
     }
 }
