3a366213d83d5aede229447f1d32b2db0be17540
motoyarko <motoyarko@gmail.com>
2016-07-12 21:43:03 +0300

    level15.lesson12

---
+++ src/com/javarush/test/level15/lesson12/home05/SubSolution.java
@@ -0,0 +1,57 @@
+package com.javarush.test.level15.lesson12.home05;
+
+/**
+ * Created by motoy on 7/12/2016.
+ */
+public class SubSolution extends Solution
+{
+    public SubSolution(byte b)
+    {
+        super(b);
+    }
+    public SubSolution(short s)
+    {
+        super(s);
+    }
+    public SubSolution(int i)
+    {
+        super(i);
+    }
+    protected SubSolution(long L)
+    {
+        super(L);
+    }
+    protected SubSolution(float f)
+    {
+        super(f);
+    }
+    protected SubSolution(double d)
+    {
+        super(d);
+    }
+    SubSolution(char c)
+    {
+        super(c);
+    }
+    SubSolution(Object o)
+    {
+        super(o);
+    }
+    SubSolution(Double D)
+    {
+        super(D);
+    }
+
+    private SubSolution(String s)
+    {
+        super(s);
+    }
+    private SubSolution(Boolean o)
+    {
+        super(o);
+    }
+    private SubSolution(boolean D)
+    {
+        super(D);
+    }
+}
