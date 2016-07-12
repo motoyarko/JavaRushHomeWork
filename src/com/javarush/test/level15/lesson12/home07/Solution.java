d6f7e6c5465a1a4dca141d71753365e85e7f34d2
motoyarko <motoyarko@gmail.com>
2016-07-12 23:59:39 +0300

    level15.lesson12

---
+++ src/com/javarush/test/level15/lesson12/home07/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.test.level15.lesson12.home07;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/* Файл в статическом блоке
+1. Инициализируй константу Constants.FILE_NAME полным путем к файлу с данными, который содержит несколько строк.
+2. В статическом блоке считай из файла с именем Constants.FILE_NAME все строки и добавь их по-отдельности в List lines.
+3. Закрой поток ввода методом close().
+*/
+
+public class Solution {
+    public static List<String> lines = new ArrayList<String>();
+
+    static {
+        try
+        {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(Constants.FILE_NAME)));
+            while (true){
+                String s = bufferedReader.readLine();
+                if (s != null){
+                    lines.add(s);
+                }else break;
+            }
+            bufferedReader.close();
+        }catch (Exception e)
+        {
+            System.out.println(e);
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(lines);
+    }
+}
