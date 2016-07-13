27d7cd7f0e8f0d67dc5ae5198ac5bc4268d61a1d
motoyarko <motoyarko@gmail.com>
2016-07-13 23:25:14 +0300

    Level15Lesson12

---
+++ src/com/javarush/test/level15/lesson12/bonus01/Helicopter.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level15.lesson12.bonus01;
+
+/**
+ * Created by motoyarko on 7/13/2016.
+ */
+public class Helicopter implements Flyable
+{
+    @Override
+    public void fly()
+    {
+
+    }
+}
---
+++ src/com/javarush/test/level15/lesson12/bonus01/Plane.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level15.lesson12.bonus01;
+
+/**
+ * Created by motoyarko on 7/13/2016.
+ */
+public class Plane implements Flyable
+{
+    int passagers;
+    public Plane(int passagers)
+    {
+        this.passagers = passagers;
+    }
+
+
+
+    @Override
+    public void fly()
+    {
+
+    }
+}
