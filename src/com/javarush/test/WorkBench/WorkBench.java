28bbf7814bcff2176ec125122f8a4d9f88c716af
Yaroslav <motoyarko@gmail.com>
2016-10-26 17:51:20 +0300

    level19.lesson03.task04

--- src/com/javarush/test/WorkBench/WorkBench.java
+++ src/com/javarush/test/WorkBench/WorkBench.java
@@ -8,7 +8,7 @@ public class WorkBench
     public static void main(String[] args){
 
 
-        String phone = "668551614";
+        String phone = "";
         String code = "38";
         while (phone.length() < 10)
         {
@@ -22,5 +22,27 @@ public class WorkBench
         out.insert(11, "-");
         out.insert(14, "-");
         System.out.println(out.toString());
+
+
+        String phoneNumber = phone;
+        String nulls = "";
+        if (phoneNumber.length()<10){
+            for (int i = 0; i < (10 - phoneNumber.length()); i++)
+            {
+                nulls = nulls + "0";
+            }
+            phoneNumber = nulls + phoneNumber;
+        }
+
+        String firstPart = phoneNumber.substring(0,3);
+        String secondPart = phoneNumber.substring(3,6);
+        String thirdPart = phoneNumber.substring(6,8);
+        String fourthPart = phoneNumber.substring(8);
+
+        System.out.println("+" + code + "(" + firstPart + ")" + secondPart + "-" + thirdPart + "-" +fourthPart);
+
+
+
+
     }
 }
---
+++ src/com/javarush/test/level19/lesson03/task04/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.test.level19.lesson03.task04;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+/* И еще один адаптер
+Адаптировать Scanner к PersonScanner.
+Классом-адаптером является PersonScannerAdapter.
+Данные в файле хранятся в следующем виде:
+Иванов Иван Иванович 31 12 1950
+
+В файле хранится большое количество людей, данные одного человека находятся в одной строке. Метод read() должен читать данные одного человека.
+*/
+
+public class Solution {
+    public static class PersonScannerAdapter implements PersonScanner{
+        private Scanner scanner;
+
+        public PersonScannerAdapter(Scanner scanner)
+        {
+            this.scanner = scanner;
+        }
+
+        @Override
+        public Person read() throws IOException
+        {
+            String line = scanner.nextLine();
+            String[] lines = line.split(" ");
+            String firstName = lines[1];
+            String middleName = lines[2];
+            String lastName = lines[0];
+            int day = Integer.parseInt(lines[3]);
+            int month = Integer.parseInt(lines[4]) - 1;
+            int year = Integer.parseInt(lines[5]);
+            Calendar birthDate = new GregorianCalendar(year, month, day);
+            return new Person(firstName, middleName, lastName, birthDate.getTime());
+        }
+
+        @Override
+        public void close() throws IOException
+        {
+        this.scanner.close();
+        }
+    }
+}
