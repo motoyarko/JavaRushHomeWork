3adf4e67f433f1a91e65377b63e81f82b6a99553
Yaroslav <motoyarko@gmail.com>
2016-11-01 15:29:35 +0200

    level19.lesson08.task03

---
+++ src/com/javarush/test/level19/lesson08/task03/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level19.lesson08.task03;
+
+/* Выводим только цифры
+В методе main подмените объект System.out написанной вами ридер-оберткой по аналогии с лекцией
+Ваша ридер-обертка должна выводить только цифры
+Вызовите готовый метод printSomething(), воспользуйтесь testString
+Верните переменной System.out первоначальный поток
+Вывести модифицированную строку в консоль.
+
+Пример вывода:
+12345678
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream originalSystemOut = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream tempSystemOut = new PrintStream(byteArrayOutputStream);
+        System.setOut(tempSystemOut);
+        testString.printSomething();
+        System.setOut(originalSystemOut);
+        String s = byteArrayOutputStream.toString();
+        s = s.replaceAll("\\D", "");
+        System.out.println(s);
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
