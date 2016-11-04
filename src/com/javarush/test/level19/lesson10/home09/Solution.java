eff955df2c3a449d05fdacfc411647d31e2ad712
Yaroslav <motoyarko@gmail.com>
2016-11-04 15:10:42 +0200

    level19.lesson10.home09

---
+++ src/com/javarush/test/level19/lesson10/home09/Solution.java
@@ -0,0 +1,60 @@
+package com.javarush.test.level19.lesson10.home09;
+
+/* Контекстная реклама
+В методе main подмените объект System.out написанной вами реадер-оберткой
+Ваша реадер-обертка должна выводить на консоль контекстную рекламу после каждого второго println-а
+Вызовите готовый метод printSomething(), воспользуйтесь testString
+Верните переменной System.out первоначальный поток
+
+Рекламный текст: "JavaRush - курсы Java онлайн"
+
+Пример вывода:
+first
+second
+JavaRush - курсы Java онлайн
+third
+fourth
+JavaRush - курсы Java онлайн
+fifth
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution
+{
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args)
+    {
+
+        PrintStream defaultSystemOut = System.out;
+        ByteArrayOutputStream tempByteArrayForOutputStram = new ByteArrayOutputStream();
+        PrintStream newSystemOut = new PrintStream(tempByteArrayForOutputStram);
+        System.setOut(newSystemOut);
+        testString.printSomething();
+        System.setOut(defaultSystemOut);
+
+        String[] tempString = tempByteArrayForOutputStram.toString().split("\\n");
+        for (int i = 0; i < tempString.length; i++)
+        {
+            if ((i + 1) % 2 == 0)
+            {
+                System.out.println(tempString[i]);
+                System.out.println("JavaRush - курсы Java онлайн");
+            } else System.out.println(tempString[i]);
+        }
+    }
+
+    public static class TestString
+    {
+        public void printSomething()
+        {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
