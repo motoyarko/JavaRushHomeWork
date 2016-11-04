a4cc2bc3bf3c47b6cd15f2124cbfd4bcae11aa84
Yaroslav <motoyarko@gmail.com>
2016-11-04 18:43:33 +0200

    level19.lesson10.bonus01

--- src/com/javarush/test/level19/lesson10/bonus01/Solution.java
+++ src/com/javarush/test/level19/lesson10/bonus01/Solution.java
@@ -29,7 +29,7 @@ file1:         file2:             результат:(lines)
 строка4        строка4            SAME строка4
 строка5                           REMOVED строка5
 */
-
+//http://info.javarush.ru/JavaRush_tasks_discussion/2016/02/14/level19-lesson10-bonus01-%D0%93%D1%80%D0%B0%D1%84%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B5-%D0%BF%D0%BE%D1%8F%D1%81%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D1%83%D1%81%D0%BB%D0%BE%D0%B2%D0%B8%D1%8F-%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B8-.html
 public class Solution
 {
     public static List<LineItem> lines = new ArrayList<LineItem>();
